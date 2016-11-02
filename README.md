fde0c65037f41968690b966f63cec131784e56f8
ConnorHowell <connor.howell@formaplex.com>
2016-11-02 13:39:15 +0000

    Various dashboard changes/updates

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # README #
-
+[![Build status](https://ci.appveyor.com/api/projects/status/djmjy08jgrgno8bi?svg=true)](https://ci.appveyor.com/project/ConnorHowell/softworkstbsmiddleware)
 Middleware to bridge TBS clocks with Softworks TNA system
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -13,47 +13,70 @@ function getCookie(cname) {
     return "";
 }
 
+function filterClockResults(array, prop, value){
+    var filtered = [];
+    for(var i = 0; i < array.length; i++){
+        var obj = array[i];
+        for (var i = 0; i < obj.length; i++) {
+            if (obj[prop] == value) {
+                filtered.push(obj);
+            }
+        }
+    }
+    return filtered;
+}
+
 function deleteAllCookies() {
     var cookies = document.cookie.split(";");
 
     for (var i = 0; i < cookies.length; i++) {
-    	var cookie = cookies[i];
-    	var eqPos = cookie.indexOf("=");
-    	var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-    	document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+        var cookie = cookies[i];
+        var eqPos = cookie.indexOf("=");
+        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
     }
 }
 
+function getClockValues () {
+    $.ajax({
+        url: '/getClockData',
+        type: 'POST',
+        contentType: 'application/json'
+    }).done(function(data) {
+        console.log(filterClockResults(data, 9, "roope"));
+    });
+}
+
 $(document).ready(function() {
-  $('.modal-trigger').leanModal();
-  $.ajax({
-      url: '/isValidToken',
-      type: 'POST',
-      contentType: 'application/json',
-      data: JSON.stringify({'sessionID':getCookie('sessionID')})}
-  ).done(function(data) {
-      if(data.code !== 1)
-      {
-          window.location.replace("/login");
-      }
-  });
+    $('.modal-trigger').leanModal();
+    $.ajax({
+        url: '/isValidToken',
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify({'sessionID':getCookie('sessionID')})}
+    ).done(function(data) {
+        if(data.code !== 1)
+        {
+            window.location.replace("/login");
+        }
+    });
 });
 (function () {
-  $('.button-collapse').sideNav();
-  $('.currentTime').text(moment().format('DDMMYYYYHHmm'));
-  $('.loading img').fadeIn(600).removeClass('hide');
-  var bootTimeout = window.setTimeout(function setBootTimeout() {
-    $('.page').removeClass('hide');
-    $('.loading').fadeOut(1000);
-    window.clearTimeout(bootTimeout);
-  }, 2000)
-  , toastTimeout = window.setTimeout(function setToastTimeout() {
-    Materialize.toast('Welcome back Admin', 4000);
-    window.clearTimeout(toastTimeout);
-  }, 3000)
-  , secondToastTimeout = window.setTimeout(function setSecondToastTimeout() {
-    window.clearTimeout(secondToastTimeout);
-  }, 5000);
+    $('.button-collapse').sideNav();
+    $('.currentTime').text(moment().format('DDMMYYYYHHmm'));
+    $('.loading img').fadeIn(600).removeClass('hide');
+    var bootTimeout = window.setTimeout(function setBootTimeout() {
+        $('.page').removeClass('hide');
+        $('.loading').fadeOut(1000);
+        window.clearTimeout(bootTimeout);
+    }, 2000)
+    , toastTimeout = window.setTimeout(function setToastTimeout() {
+        Materialize.toast('Welcome back Admin', 4000);
+        window.clearTimeout(toastTimeout);
+    }, 3000)
+    , secondToastTimeout = window.setTimeout(function setSecondToastTimeout() {
+        window.clearTimeout(secondToastTimeout);
+    }, 5000);
 })();
 
 function showHelp(field) {
@@ -80,19 +103,23 @@ function viewContents(filename) {
     ).done(function(data) {
         $('.csvContents').empty();
         Papa.parse(data, {
-        	complete: function(results) {
+            complete: function(results) {
                 var tableRows;
-        		results.data.forEach(function (row) {
+                results.data.forEach(function (row) {
                     tableRows += '<tr><td>'+row[0]+'</td>'+'<td>'+row[3]+'</td>'+'<td>'+row[4]+'</td>'+'<td>'+row[5]+'</td></tr>';
                 });
                 $('.csvContents').html(tableRows);
-        	}
+            }
         });
     });
 }
 
 function logoutDialog() {
     $('#logoutDialog').openModal();
+}
+
+function licenseInfo() {
+    $('#licenseDialog').openModal();
 }
 
 function performLogout() {
@@ -196,7 +223,8 @@ function refreshValue() {
 }
 
 var data = {
-  series: [5, 3, 4]
+    labels: ['Clocking', 'Enrollment', 'Admin'],
+    series: [5, 3, 4]
 };
 
 var sum = function(a, b) { return a + b };
@@ -204,20 +232,17 @@ var sum = function(a, b) { return a + b };
 new Chartist.Pie('.ct-chart', data, {
     width: '400px',
     height: '400px',
-  labelInterpolationFnc: function(value) {
-    return Math.round(value / data.series.reduce(sum) * 100) + '%';
-  }
 });
 
 new Chartist.Line('.clock-chart', {
-  labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
-  series: [
-    [12, 9, 7, 8, 5]
-  ]
+    labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+    series: [
+        [0, 0, 0, 0, 0]
+    ]
 }, {
-  width: '900px',
-  height: '400px',
-  chartPadding: {
-    right: 40
-  }
+    width: '900px',
+    height: '400px',
+    chartPadding: {
+        right: 40
+    }
 });
